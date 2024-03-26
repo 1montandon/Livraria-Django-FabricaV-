95858fd8eb453a79c9ddaa4f0de1c44e56994645
1montandon <emontandon11@gmail.com>
2024-03-26 16:22:42 -0300

    Formatação dos arquivos do projeto - Ate Aula 4.7

--- livraria/models.py
+++ livraria/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return self.descricao
-    
+
+
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(null=True, blank=True)
