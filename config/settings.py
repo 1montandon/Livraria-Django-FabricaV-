5229ad77f58ab232f2a71de59167d1c8e6f211f9
1montandon <emontandon11@gmail.com>
2024-03-27 12:56:45 -0300

    Instalação do DRF e Criação da API REST - Ate Aula 8.0

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'livraria',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from livraria.views import CategoriaViewSet
+
+router = DefaultRouter()
+router.register(r"categorias", CategoriaViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]
