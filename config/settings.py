531bbb3e4228e7ee82b9e04cd48c41a79253a555
1montandon <emontandon11@gmail.com>
2024-03-27 12:33:38 -0300

    Criando outras models e incluindo chaves estrangeiras - Até aula 7.0

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'America/'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- livraria/admin.py
+++ livraria/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from livraria.models import Categoria, Editora
+from livraria.models import Categoria, Editora, Autor, Livro 
 
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Livro)
+admin.site.register(Autor)
